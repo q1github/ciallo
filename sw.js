9d1d10d33aef55f0c89f85e4ba3f1282803d6e22
q1github <3391584886@qq.com>
2024-12-08 16:22:26 +0800

    Site updated: 2024-12-08 16:22:24

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1733646092154";const preCache = ["/ciallo/images/taichi.png","/ciallo/images/banner.webp","/ciallo/css/loader.css","/ciallo/css/style.css","/ciallo/js/script.js"];const cacheDomain = [
+const VERSION = "1733646139230";const preCache = ["/ciallo/images/taichi.png","/ciallo/images/banner.webp","/ciallo/css/loader.css","/ciallo/css/style.css","/ciallo/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
